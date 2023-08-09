54328129935fad25a4688af45c76e95c0c0e7d54
ShahidR-np <116190347+ShahidR-np@users.noreply.github.com>
2023-08-09 20:59:03 +0800

    Update INVEMP.py

--- INVEMP.py
+++ INVEMP.py
@@ -15,31 +15,19 @@ st.sidebar.title("INVEMP: Inventory/Warehouse Management & Prediction on Sales p
 st.sidebar.markdown("This web app allows you to explore the internal inventory of Tasty Bytes. You can explore these functions in the web app (Description of Page)")
 
 @st.cache_data
-
-def load_v1():
-    acd = pd.read_csv("./allcustdatav1.csv")
-    return acd
-
-def load_v2():
-    acd = pd.read_csv("./allcustdatav2.csv")
-    return acd
-
 def load_orderdata():
      od = pd.read_csv("./orderdatav2.csv")
      return od
 
-#custdatav1 = load_v1()
-#custdatav2 = load_v2()
-#orderdata = load_orderdata()
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Churn Prediction', 'Customer Revenue Calculation', 'Bundled Items Sales Analysis', 'Prediction D', 'Prediction E'])
 
-#churndata = load_churn_data()
-
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Churn Prediction', 'Revenue Calculation', 'Prediction C', 'Prediction D', 'Prediction E'])
-
-with tab1:
-    st.write("Tab1")
 with tab2:
-     
+     st.title('💲 Calculation of Revenue 💲')
+     st.markdown("This tab predicts whether or not the customers in a selected cluster is likely to churn. It also includes insights on the selected cluster, such as their total revenue by year \
+                 as well as the number of orders made by this cluster for each menu type. \nAt the bottom, there is a revenue calculation to estimate the revenue by this cluster \
+                 in the following year if they do not churn. This calculation is based on the cluster's revenue generated in the previous years.")
+     st.markdown('________________________________________________')
+
      def read_csv_from_zipped_github(url):
     # Send a GET request to the GitHub URL
         response = requests.get(url)
@@ -61,8 +49,8 @@ with tab2:
             st.error(f"Failed to retrieve data from {url}. Status code: {response.status_code}")
             return None
     
-     #gu_acd  =  "https://github.com/ShahidR-np/testzipfiles/raw/main/allcustdata.zip"
-     #custdata = read_csv_from_zipped_github(gu_acd)
+     # gu_acd  =  "https://github.com/ShahidR-np/testzipfiles/raw/main/allcustdata.zip"
+     # custdata = read_csv_from_zipped_github(gu_acd)
      gu_od = "https://github.com/ShahidR-np/testzipfiles/raw/main/orderdatav2.zip"
      orderdata = read_csv_from_zipped_github(gu_od)
 
@@ -93,14 +81,12 @@ with tab2:
      freq_val = freq_dict[frequency_level_t2]
      spend_val = spend_dict[spending_level_t2]
      hist_val = hist_dict[history_level_t2]
-
      if hist_val == 0:
         od = pd.read_csv("./custdatav0.csv")
      elif hist_val == 1:
         od = pd.read_csv("./custdatav1.csv")
      elif hist_val == 2:
         od = pd.read_csv("./custdatav2.csv")
-
      #Filtering data based on clusters
      #v1filtered = custdatav1[(custdatav1['sale_cluster'] == spend_val) & (custdatav1['Customer_age_cluster'] == hist_val) & (custdatav1['frequency_cluster'] == freq_val )]
      #v2filtered = custdatav2[(custdatav2['sale_cluster'] == spend_val) & (custdatav2['Customer_age_cluster'] == hist_val) & (custdatav2['frequency_cluster'] == freq_val )]
@@ -117,7 +103,7 @@ with tab2:
      st.write("Number of orders by menu type")
      st.table(gbmt)
 
-     #Model and Prediction
+     # Model and Prediction
      with open('cdc_xgb.pkl', 'rb') as file:
          cdcxgb = pickle.load(file)
      
@@ -127,18 +113,19 @@ with tab2:
 
 
      predictedchurn=cdcxgb.predict(clustermode[['TOTAL_PRODUCTS_SOLD', 'ORDER_AMOUNT', 'TOTAL_ORDERS',
-      'MIN_DAYS_BETWEEN_ORDERS', 'MAX_DAYS_BETWEEN_ORDERS',
-      'frequency_cluster', 'Customer_age_cluster', 'sale_cluster',
-      'CITY_Boston', 'CITY_Denver', 'CITY_New York City', 'CITY_San Mateo',
-      'CITY_Seattle', 'REGION_California', 'REGION_Colorado',
-      'REGION_Massachusetts', 'REGION_New York', 'REGION_Washington',
-      'MENU_TYPE_BBQ', 'MENU_TYPE_Chinese', 'MENU_TYPE_Crepes',
-      'MENU_TYPE_Ethiopian', 'MENU_TYPE_Grilled Cheese', 'MENU_TYPE_Gyros',
-      'MENU_TYPE_Hot Dogs', 'MENU_TYPE_Ice Cream', 'MENU_TYPE_Indian',
-      'MENU_TYPE_Mac & Cheese', 'MENU_TYPE_Poutine', 'MENU_TYPE_Ramen',
-      'MENU_TYPE_Sandwiches', 'MENU_TYPE_Tacos', 'MENU_TYPE_Vegetarian']])
-     #predictedchurn=cdcxgb.predict([1, 11, 60, 1, 83, freq_val, hist_val, spend_val, 0, 0, 0,0 ,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+       'MIN_DAYS_BETWEEN_ORDERS', 'MAX_DAYS_BETWEEN_ORDERS',
+       'frequency_cluster', 'Customer_age_cluster', 'sale_cluster',
+       'CITY_Boston', 'CITY_Denver', 'CITY_New York City', 'CITY_San Mateo',
+       'CITY_Seattle', 'REGION_California', 'REGION_Colorado',
+       'REGION_Massachusetts', 'REGION_New York', 'REGION_Washington',
+       'MENU_TYPE_BBQ', 'MENU_TYPE_Chinese', 'MENU_TYPE_Crepes',
+       'MENU_TYPE_Ethiopian', 'MENU_TYPE_Grilled Cheese', 'MENU_TYPE_Gyros',
+       'MENU_TYPE_Hot Dogs', 'MENU_TYPE_Ice Cream', 'MENU_TYPE_Indian',
+       'MENU_TYPE_Mac & Cheese', 'MENU_TYPE_Poutine', 'MENU_TYPE_Ramen',
+       'MENU_TYPE_Sandwiches', 'MENU_TYPE_Tacos', 'MENU_TYPE_Vegetarian']])
+  
      
+     #predictedchurn = 1
      churntext = ""
      if (predictedchurn == 1):
           churntext = "LESS"
@@ -177,12 +164,7 @@ with tab2:
 
      st.header("Revenue Calculation")
      st.write("- In the following year, this group of customer is predicted to have an increase of {1:.2f}% in revenue sales, which is a total revenue of {0:.2f}".format(odgb2023, percinc2023 ))
-
-
-
-
-
-
+  
 
 
 
